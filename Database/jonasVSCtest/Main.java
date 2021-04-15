157be086634480577e7a19249f98d58c5aec650d
X1las <dahepix1@gmail.com>
2021-04-15 15:08:09 +0200

    minor tweaks, prepping query method

--- Database/jonasVSCtest/Main.java
+++ Database/jonasVSCtest/Main.java
@@ -1,7 +1,15 @@
 
 public class Main {
     public static void main(String[] args) {
+        conclear(10); //Temporary command to add space in console
         String db = "jdbc:sqlite:/home/xilas/Desktop/gits/Portfolios/Database/Main/StudentsDB.db";
         jdbc con = new jdbc(db);
+    }
+    public static void conclear(int times)
+    {
+        for (int i = 0 ; i < times ; i++)
+        {
+            System.out.println("\n");
+        }
     }
 }
--- Database/jonasVSCtest/jdbc.java
+++ Database/jonasVSCtest/jdbc.java
@@ -1,16 +1,20 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class jdbc 
 {
-  Connection conn = null;
+  // Private variables for connector and database
+  private Connection conn = null;
+  private String db = null;
 
+  // Init with test connection
   public jdbc(String database)
   {
     System.out.println("Attempting connect to database at: " + database);
     try
     {
-      conn = DriverManager.getConnection(database);
-      System.out.println("Database Connected Successfully!");
+      connect(database);
+      db = database;
     }
     catch (SQLException e)
     {
@@ -18,13 +22,53 @@ public class jdbc
     }
     finally
     {
-      if (conn != null) {
-        try {
-          conn.close();
-        } catch (SQLException ex) {
-          System.out.println(ex.getMessage());
-        }
+      endConnection();
+    }
+  }
+
+  // Method for connecting the jdbc driver to database through the object, only use internally
+  private void connect(String database) throws SQLException
+  {
+    conn = DriverManager.getConnection(database);
+    System.out.println("Database Connected Successfully!");
+  }
+
+  // Default method using internal database
+  private void connect() throws SQLException
+  {
+    if (db != null)
+    {
+      connect(db);
+    } 
+    else
+    {
+      System.err.println("Connector error: Internal database missing or not set");
+    }
+  }
+
+  public void endConnection()
+  {
+    if (conn != null) {
+      try {
+        conn.close();
+      } catch (SQLException ex) {
+        System.err.println(ex.getMessage());
       }
     }
+  }
+
+  public ArrayList query(String Query)
+  {
+    ArrayList result = null;
+    try {
+      connect();
+    } catch (SQLException e) {
+      System.err.println("Error: query refused connect");
+      e.printStackTrace();
+    } finally
+    {
+      endConnection();
+    }
+    return result;
   }
 }
