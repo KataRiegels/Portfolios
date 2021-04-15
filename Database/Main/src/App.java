f6608be35eea9f0468e9a7fa191e245adf9b7f46
KataRiegels <KataRiegels@users.noreply.github.com>
2021-04-15 14:57:04 +0200

    init commit

--- Database/Main/src/App.java
+++ Database/Main/src/App.java
@@ -4,6 +4,44 @@ import static java.sql.DriverManager.getConnection;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String query = "select studentName,grade from CourseGrade ";
+        Connection conn = null;
+
+        try{
+            conn = connect();
+            poop(conn, query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn!=null){
+                try{
+                    conn.close();
+                } catch (SQLException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
+    public static Connection connect() throws SQLException{
+        Connection conn = null;
+        String url = "jdbc:sqlite:C:\\Users\\Mitzie\\Documents\\4th_sem\\Software development\\Portfolios\\Database\\Main\\StudentsDB.db";
+        conn = DriverManager.getConnection(url);
+        return conn;
+    }
+
+    public static void poop(Connection conn, String query){
+        try (Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String studentName = rs.getString("studentName");
+                System.out.println(studentName);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
     }
 }
