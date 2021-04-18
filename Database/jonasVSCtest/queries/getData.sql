254f3aa4d789299c07df86e8d41c8b532ebcae99
X1las <dahepix1@gmail.com>
2021-04-18 21:03:04 +0200

    Discovered SQL search parameters

--- Database/jonasVSCtest/queries/getData.sql
+++ Database/jonasVSCtest/queries/getData.sql
@@ -1,2 +1,16 @@
--- SQLite
-SELECT * FROM Towns
+-- Make a user interface in javaFX where a user can select a student 
+-- and get print out courses taken and the grades and the average grade for student 
+-- and a course and get average grade on a selected course.
+SELECT studentName,grade,Grades.courseID,courseName,courseYear,fallSemester,teacherName 
+FROM Grades 
+INNER JOIN Courses ON Grades.courseID=Courses.courseID 
+WHERE studentName='Aisha Lincoln';
+
+SELECT AVG(grade) as Average 
+FROM Grades 
+WHERE studentName='Aisha Lincoln';
+
+SELECT AVG(grade) as CourseAverage 
+FROM Grades 
+INNER JOIN Courses ON Grades.courseID=Courses.courseID 
+WHERE courseName='Software Development';
