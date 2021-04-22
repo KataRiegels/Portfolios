ffffad9bb2dc056abc1d51d01eb2c34392ab89e1
KataRiegels <KataRiegels@users.noreply.github.com>
2021-04-22 10:56:42 +0200

    we rdy for thursday yo

--- Database/Main/src/StudentsView.java
+++ Database/Main/src/StudentsView.java
@@ -6,17 +6,18 @@ import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.text.Text;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextInputControl;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 
 import java.util.ArrayList;
 
 
 public class StudentsView {
     private StudentsController control;
-    private GridPane Startview;
+    private Pane Startview;
     Button exitBtn   = new Button("Exit");
     Button returnBTN = new Button("Return");
 
@@ -50,19 +51,32 @@ public class StudentsView {
 
 
     public StudentsView(StudentsController control){
-        this.control=control;
+        this.control = control;
         createAndConfigure();
     }
 
     private void createAndConfigure(){
-        Startview = new GridPane();
-        Startview.setMinSize(300,200);
-        Startview.setPadding(new Insets(10,10,10,10));
-        Startview.setVgap(5);
-        Startview.setHgap(1);
+        Startview = new Pane();
+        Startview.setMinSize(500,500);
 
+        //Startview.setPadding(new Insets(10,10,10,10));
+        //Startview.setVgap(10);
+        //Startview.setHgap(10);
+        //Startview.setGridLinesVisible(true);
+        /*ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(50);
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(50);
+        ColumnConstraints column3 = new ColumnConstraints();
+        column3.setPercentWidth(50);*/
+        //Startview.getColumnConstraints().addAll(column1, column2,column3); // each get 50% of width
 
-        Startview.add(exitBtn, 20,40);
+        Node[] screen1 = {continueBTN, studentOrCourseLBL, selStudentOrCourseCOMB};
+        Node[] screen2 = { selectCourseLBL, selectCourseCOMB,displayCourseInfoTXT, confirmCourseBTN,returnBTN};
+        Node[] screen3 = {selectStudentLBL, selectStudentCOMB, displayStudentInfoTXT, addGradeBTN, confirmStudentBTN};
+
+        Startview.getChildren().addAll(screen1);
+        //Startview.add(exitBtn, 21,2);
         /*
         Startview.add(getCourseAverageBTN,15,6);
         Startview.add(continueBTN,20,6);
@@ -92,11 +106,9 @@ public class StudentsView {
         //EndStationComB.getSelectionModel().selectFirst();
 
          */
-
-
-
-
     }
+
+
 
     public Parent asParent(){
         return Startview;
